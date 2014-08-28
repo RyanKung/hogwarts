3bd73218d852d6c087564961aa2d39f26f41849b
Ryan Kung <ryan.kung@imaygou.com>
2014-08-28 18:46:25 +0800

    project

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/java.jdbc "0.3.2"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [clojurewerkz/neocons"3.0.0"]]
   :main ^:skip-aot hogwarts.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
