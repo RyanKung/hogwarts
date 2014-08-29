370d3ef6c2a687946e89979cb37a8fa1b64f5a6e
Ryan Kung <ryan.kung@imaygou.com>
2014-08-29 13:36:33 +0800

    template

--- src/hogwarts/core.clj
+++ src/hogwarts/core.clj
@@ -1,11 +1,10 @@
 (ns hogwarts.core
   (:use ring.adapter.jetty
         ring.util.response
-        lightbringer.view)
+        hogwarts.view)
 
   (:require [net.cgrand.enlive-html :as html])
 )
-
 (defn -main
   [& args]
   (run-jetty router {:port 8080}))
--- src/hogwarts/view.clj
+++ src/hogwarts/view.clj
@@ -1,16 +1,16 @@
-(ns lightbringer.view
+(ns hogwarts.view
   (:use ring.util.response)
   (:require [net.cgrand.enlive-html :as html])
 )
 
 (html/deftemplate main-template "templates/index.html"
-  [str]
-  [:html] (html/content str))
+  []
+)
 
 (defn router [request]
   (condp = (:uri request)
     "/"
-    (-> (response (apply str (main-template (:uri request))))
+    (-> (response (apply str (main-template)))
         (content-type "text/html"))
 
     (-> (response "404")
